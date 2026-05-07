6ceb80633ba7c2e5a88a182f721c6718d80fd6f8
Alfonso Mayzart <alfonsomzrt@gmail.com>
2026-05-07 15:46:21 +0700

    Refactor app.js for improved functionality and clarity

--- app.js
+++ app.js
@@ -269,9 +269,9 @@ function downloadCSV() {
   }).replace(':', '-');
 
 //Susun isi CSV 
-  let csvContent = "'Timestamp','SPL (dBA)'\n";
+  let csvContent = '"Timestamp","SPL (dBA)"\n';
   logBuffer.forEach(row => {
-    csvContent += `'${row.time}', '${row.spl}'\n`;
+    csvContent += `"${row.time}","${row.spl}"\n`;
   });
 
   const blob = new Blob([csvContent], {type: 'text/csv;charset=utf-8;' });
