11f397f281190ba0abaa9679eb7bbbf225bfd963
Alfonso Mayzart <alfonsomzrt@gmail.com>
2026-05-12 13:37:21 +0700

    Update MQTT broker configuration parameters

--- overview.js
+++ overview.js
@@ -3,9 +3,9 @@
 // =========================
 //sebagai contoh, ubah sendiri sesuai konfigurasi HIVEMQ MQTT broker
 const BROKER = "b8ae4809915f4027b2d18c7fc2109b204.s1.eu.hivemq.cloud";
-const PORT = 8881;
-const USER = "esp32-v2";
-const PASS = "RajaSawit_2021";
+const PORT = 8883;
+const USER = "esp32-v1";
+const PASS = "RajaSawit_2026";
 
 const TIMEOUT = 10000; // ms → dianggap offline
 
