70aaa449c612da6c1dfe5e907b3f2c200fac66d7
Alfonso Mayzart <alfonsomzrt@gmail.com>
2026-05-14 08:53:00 +0700

    Refactor MQTT configuration
    Refactor app.js to use placeholders for MQTT configuration and improve code structure.

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 // =========================
 // CONFIG
 // =========================
-const BROKER = "b8ae4809915f4027b2d18c7fc219b204.s1.eu.hivemq.cloud";
-const PORT = 8884;
-const USER = "esp32-v1";
-const PASS = "RajaSawit_2026";
+const BROKER = "__MQTT_BROKER__";
+const PORT = __MQTT_PORT__;
+const USER = "__MQTT_USER__";
+const PASS = "__MQTT_PASS__";
 
 const MAX_POINTS = 30;
 const TIMEOUT = 5000; // ms
